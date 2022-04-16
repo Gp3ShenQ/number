dca49f3239067e59a0470a27cedbae88f7b6306b
ShenQ <gp3g4xup6@gmail.com>
2022-04-17 00:15:23 +0800

    new

--- number.py
+++ number.py
@@ -1,12 +1,11 @@
 import random
 
-r = random.randint(1,3)
+r = random.randint(1,500)
 num = 0
 while True:
     num += 1
     print('進行第', num ,'次猜測!') 
     i = int(input('輸入猜的數字: '))
-   # print(r) #隨機數  看這邊  方便測試程式
     if r == i:
         print('猜對囉!!!')
         break
