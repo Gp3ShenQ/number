f1fe639d81c3f2b734eaf51e8c4c0c5447ad59f2
ShenQ <gp3g4xup6@gmail.com>
2022-04-16 23:55:45 +0800

    new

--- 123.py
+++ 123.py
@@ -1,15 +1,18 @@
 import random
 
-# r = random.randint(1,3)
-# i = int(input('輸入猜的數字: '))
-# print('正確答案',r)
-
+r = random.randint(1,3)
+num = 0
 while True:
-    r = random.randint(1,3)
+    num += 1
+    print('進行第', num ,'次猜測!') 
     i = int(input('輸入猜的數字: '))
-    print('正確答案',r)
+   # print(r) #隨機數  看這邊  方便測試程式
     if r == i:
         print('猜對囉!!!')
         break
-    else:
-        print('答錯!')
+    elif i < r :
+        print('數字再大一點!')
+    elif i > r :
+        print('數字在小一點!')
+
+print('在第', num ,'次終於猜對了!!!!!!!!')       
